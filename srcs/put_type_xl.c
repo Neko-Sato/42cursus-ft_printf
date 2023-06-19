c6de30b7752afc5440d70f89ffef291945919210
Neko-Sato <hisatosi35350@gmail.com>
2023-06-19 19:58:28 +0900

    printf完了疲れち、、、

--- srcs/put_type_xl.c
+++ srcs/put_type_xl.c
@@ -45,50 +45,31 @@ static size_t	_put_hex(unsigned long n, int large, size_t digit)
 	return (buf_write_stdout(buf, digit));
 }
 
-static size_t	_put_type_xl(unsigned long n, int flag, int width,
-		int precision)
-{
-	size_t	ret;
-	size_t	len;
-
-	ret = 0;
-	len = ft_ultohlen(n);
-	if (precision != PRECISION_DEFAULT)
-	{
-		if ((int)len < precision || !n)
-			len = precision;
-		else
-			flag &= ~FLAG_ZERO;
-	}
-	ret += _put_width(flag, width, len, 0);
-	if (precision && precision != PRECISION_DEFAULT)
-		ret += _put_type_xl(n, (flag & ~FLAG_MINUS) | FLAG_ZERO, precision, PRECISION_DEFAULT);
-	else if (len)
-		ret += _put_hex(n, !!(flag & X_LARGE), len);
-	ret += _put_width(flag, width, len, 1);
-	return (ret);
-}
-
 size_t	put_type_xl(unsigned long n, int flag, int width, int precision)
 {
 	int		isprefix;
-	int		_width;
 	size_t	ret;
 	size_t	len;
 
 	ret = 0;
-	isprefix = (!!(flag & (FLAG_HASH | X_IS_P)));
+	isprefix = ((flag & FLAG_HASH && n) || (flag & X_IS_P));
 	if (width != WIDTH_DEFAULT)
 		width -= 2 * isprefix;
 	len = ft_ultohlen(n);
-	_width = WIDTH_DEFAULT;
-	if (!(flag & FLAG_ZERO))
-		ret += _put_width(flag, width, len, 0);
-	else
-		_width = width;
-	if (precision && (n || flag & X_IS_P) && isprefix)
+	if (precision != PRECISION_DEFAULT)
+	{
+		flag &= ~FLAG_ZERO;
+		if ((int)len < precision || !n)
+			len = precision;
+	}
+	ret += _put_width(flag, width, len, 0);
+	if (isprefix)
 		ret += buf_write_stdout(&"0x0X"[2 * !!(flag & X_LARGE)], 2);
-	ret += _put_type_xl(n, flag, _width, precision);
+	if (precision && precision != PRECISION_DEFAULT)
+		ret += put_type_xl(n, (flag & ~FLAG_MINUS) | FLAG_ZERO, precision,
+				PRECISION_DEFAULT);
+	else if (len)
+		ret += _put_hex(n, !!(flag & X_LARGE), len);
 	ret += _put_width(flag, width, len, 1);
 	return (ret);
 }
