951d0deac545e1dfa27ec1d9ea31fe80dca24215
Uru Aki <uruaki@tsuki-2.local>
2025-09-07 16:11:17 +0900

    update

--- ft_varg_0_ref.c
+++ ft_varg_0_ref.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_varg_0_apple.c                                  :+:      :+:    :+:   */
+/*   ft_varg_0_ref.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hshimizu <hshimizu@42tokyo.student.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 15:36:52 by hshimizu          #+#    #+#             */
-/*   Updated: 2025/09/07 16:05:24 by hshimizu         ###   ########.fr       */
+/*   Updated: 2025/09/07 16:10:43 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_varg.h>
 
-#if defined(_VARG_REF)
+#if _VARG_REF == 1
 
 signed int	ft__va_arg_signed_int(t__va_list_ref ap)
 {
